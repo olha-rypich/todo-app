0251fab644e1c165c496cf0a185317d3b5f8bc74
Olha Rypich <olha.rypich@gmail.com>
2024-12-28 01:50:55 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TodoApp } from './components/TodoApp';
+import { TodosProvider } from './components/TodosContext';
+
+export const App: React.FC = () => (
+  <TodosProvider>
+    <TodoApp />
+  </TodosProvider>
+);
